36f83d59156f7b4e55ab488664c57124448efd83
swapme <athawale.1@iitj.ac.in>
2017-03-11 13:53:28 +0530

    add request

--- scripts/network.js
+++ scripts/network.js
@@ -2,18 +2,13 @@ var account = "extension-westworld";
 var repository = "e3bd020e-a357-42f3-8da2-fb5880201dbd";
 
 var url = "https://" + account + ".visualstudio.com/DefaultCollection/_apis/git/repositories/";
-request += repository + "/commits?api-version=1.0";
+url += repository + "/commits?api-version=1.0";
 
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.open("GET", url, false);
-xmlHttp.send();
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url, true);
 
-if (xmlHttp.readyState == 4 && xmlHttp.sstatus == 200)
-{
-  var response = JSON.parse(xmlHttp.responseText);
-  alert(response);
-}
-else
-{
-  alert("Nope");
-}
+xhr.open('GET', '/api', true);
+xhr.send(null);
+//var response = JSON.parse(xhr.responseText);
+console.log(xhr.responseText);
+
