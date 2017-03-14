ead8fa0f88b7906208f2a889cddefff2d59dad4d
flomonster <florian.amsallem@epita.fr>
2017-03-14 07:57:24 +0100

    ADD html link

--- scripts/network.js
+++ scripts/network.js
@@ -1,14 +1,16 @@
  VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], function (VSS_Service, TFS_Wit_WebApi) {
     var witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
-    console.log(witClient);
 
-    var repository = "e3bd020e-a357-42f3-8da2-fb5880201dbd";
+    var repository = "9695907c-03cd-4876-976d-19cfe8bb4c58";
 
     var url = witClient._rootRequestPath + "DefaultCollection/_apis/git/repositories/";
-    url += repository + "/commits?api-version=1.0";
+    url += repository + "/commits?api-version=1.0&$top=1000";
+    document.getElementById("commits_url").innerHTML+= "<a href=\"" + url + "\"> COMMITS </a>";
+    console.log(witClient);
+    var token = "7amudcmr43sbrwskgsdt7uju5cguo7tdnn3k2xek4hmgcfvraxoq";
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
+    xhr.open("GET", url, true, password = btoa(token));
 
     xhr.onload = function () {
       if (xhr.status == 200)
