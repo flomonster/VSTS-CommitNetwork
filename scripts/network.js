42dc70160116bd53e58946a96c46bc92783295f3
flomonster <florian.amsallem@epita.fr>
2017-03-23 17:02:21 +0100

    ADD Some try

--- scripts/network.js
+++ scripts/network.js
@@ -1,26 +1,27 @@
- VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], function (VSS_Service, TFS_Wit_WebApi) {
-    var witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+VSS.require(["VSS/Service", "VSS/Authentication/Services", "TFS/VersionControl/GitRestClient"], function (VSS_Service, AuthenticationService, TFS_Wit_WebApi) {
+    /*
+    var authTokenManager = AuthenticationService.authTokenManager; 
+    authTokenManager.getToken().then(function (token) {
+      console.log(token);
+    var header = authTokenManager.getAuthorizationHeader(token);
+    var req = new XMLHttpRequest();
+    var url = "https://extension-westworld.VisualStudio.com/DefaultCollection/_apis/projects?api-version=2.0";
+    req.open("GET", url, true);
+    req.setRequestHeader("Authorization", header); 
+    req.onload = function () {
+      console.log(req.status);
+      if (req.status == 200)
+        console.log(req.responseText);
 
-    var repository = "9695907c-03cd-4876-976d-19cfe8bb4c58";
-
-    var url = witClient._rootRequestPath + "DefaultCollection/_apis/git/repositories/";
-    url += repository + "/commits?api-version=1.0&$top=1000";
-    document.getElementById("commits_url").innerHTML+= "<a href=\"" + url + "\"> COMMITS </a>";
-    console.log(witClient);
-    var token = "7amudcmr43sbrwskgsdt7uju5cguo7tdnn3k2xek4hmgcfvraxoq";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true, password = btoa(token));
-
-    xhr.onload = function () {
-      if (xhr.status == 200)
-      {
-        console.log(xhr.responseText);
-        //var response = JSON.parse(xhr.responseText);
-      }
-      else
-        console.log("STATUS : " + xhr.status);
-    };
-
-    xhr.send(null);
+    }
+    req.send();
+    });
+    */
+    var client = VSS_Service.getCollectionClient(TFS_Wit_WebApi.GitHttpClient);
+    var repoID = "9695907c-03cd-4876-976d-19cfe8bb4c58";
+    client.getCommits(repoID, null).then(
+        function(commits)
+        {
+          console.log(commits);
+        });
 });
