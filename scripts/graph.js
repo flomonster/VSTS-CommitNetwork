8a581968445fa144418bdb774c7a21716dbf00b6
flomonster <florian.amsallem@epita.fr>
2017-03-25 08:31:53 +0100

    ADD test1 graph

--- scripts/graph.js
+++ scripts/graph.js
@@ -10,3 +10,6 @@ class Graph
     this.children = children;
   }
 }
+
+child1 = new Graph("id 2", "swapme <athawale.1@iitj.ac.in>", "ADD readme", "23/03/2017", "");
+test1 = new Graph("id 1", "swapme <athawale.1@iitj.ac.in>", "Initial Commit", "22/03/2017", "", [child1]);
