de63890ff87fd0cffacd22cbabc5be790354eff5
flomonster <florian.amsallem@epita.fr>
2017-03-27 08:31:21 +0200

    ADD begin of display function

--- scripts/display.js
+++ scripts/display.js
@@ -24,232 +24,54 @@ var config = {
   orientation: "horizontal"
 };
 
-var bugFixCommit = {
-  messageAuthorDisplay: false,
-  messageBranchDisplay: false,
-  messageHashDisplay: false,
-  message: "Bug fix commit(s)"
-};
+function addChildren(stack, graph, currentBranch, gitgraph)
+{
+  if (graph.children.length)
+  {
+    stack.push([graph.children[0], branch]);
+  }
+  for (int i = 1; i < graph.children.length; i++)
+    stack.push(graph.children[i], gitgraph.branch());
+}
 
-var stabilizationCommit = {
-  messageAuthorDisplay: false,
-  messageBranchDisplay: false,
-  messageHashDisplay: false,
-  message: "Release stabilization commit(s)"
-};
+function displayGraph(graph)
+{
+  var gitgraph = new GitGraph(config);
+  var stack = [[graph, gitgraph.branch()]];
+  var tobemerge = []
+  while (stack.length)
+  {
+    [graph, branch] = stack.pop();
+    if (graph.merge)
+    {
+      var find = false;
+      for (graph2 in tobemerge)
+      {
+        if (graph2[0] == graph[0])
+        {
+          branch.merge(graph2[1], 
+              {
+                sha1: graph.id,
+                message: graph.msg,
+                author: graph.author,
+              });
+          find = true
+          break;
+        }
+      }
+      if (!find)
+        tobemerge.push([graph, branch]);
+    }
+    else
+    {
+      branch.commit(
+      {
+        sha1: graph.id,
+        message: graph.msg,
+        author: graph.author,
+      });
+    }
+  }
+}
 
-// You can manually fix columns to control the display.
-var masterCol = 0;
-var developCol = 1;
-var featureCol = 2;
-var releaseCol = 3;
-var supportCol = 4;
-
-
-var gitgraph = new GitGraph(config);
-
-var master = gitgraph.branch({
-  name: "master",
-  column: masterCol
-});
-master.commit("Initial commit");
-
-var develop = gitgraph.branch({
-  parentBranch: master,
-  name: "develop",
-  column: developCol
-});
-master.commit({
-  messageDisplay: false // for msg disaply false it is showing msg He doesn't like George Michael!
-});
-develop.commit({
-  messageDisplay: false
-});
-
-var feature1 = gitgraph.branch({
-  parentBranch: develop,
-  name: "feature/1",
-  column: featureCol
-});
-feature1.commit("A feature to go into v1.0.0");
-feature1.merge(develop);
-
-var feature2 = gitgraph.branch({
-  parentBranch: develop,
-  name: "feature/2",
-  column: featureCol
-});
-feature2.commit("Another feature to go into v1.0.0");
-feature2.merge(develop);
-
-var release_100 = gitgraph.branch({
-  parentBranch: develop,
-  name: "release/v1.0.0",
-  column: releaseCol
-});
-release_100.commit({
-  message: "Start v1.0.0-rc Release Candidate builds",
-  
-});
-
-release_100.commit(stabilizationCommit);
-release_100.merge(develop).merge(master, {// merge to two 
-  //dotStrokeWidth: 10, // to make the big circle where it is merging 
-  message: "Release v1.0.0 tagged",
-  
-});
-
-var support_10x = gitgraph.branch({
-  parentBranch: master,
-  name: "support/v1.0.x",
-  column: supportCol
-});
-support_10x.commit({
-  message: "Start v1.0.1-rc Release Candidate builds",
-  
-}).commit(bugFixCommit);
-
-var feature3 = gitgraph.branch({
-  parentBranch: develop,
-  name: "feature/3",
-  column: featureCol
-});
-develop.commit({
-  messageDisplay: false
-});
-feature3.commit("A feature to go into v1.1.0").commit({
-  messageDisplay: false
-});
-feature3.merge(develop);
-
-support_10x.commit({
-  
-  message: "Release v1.0.1 tagged",
-  
-}).merge(develop);
-
-develop.commit({
-  messageDisplay: false
-});
-support_10x.commit({
-  message: "Start v1.0.2-rc Release Candidate builds",
-  
-})
-support_10x.commit(bugFixCommit).commit({
-  
-  message: "Release v1.0.2 tagged",
- 
-});
-support_10x.merge(develop);
-develop.commit({
-  messageDisplay: false
-});
-
-var release_110 = gitgraph.branch({
-  parentBranch: develop,
-  name: "release/v1.1.0",
-  column: releaseCol
-});
-release_110.commit({
-  message: "Start v1.1.0-rc Release Candidate builds",
- 
-})
-release_110.commit(stabilizationCommit);
-release_110.merge(develop).merge(master, {
- 
-  message: "Release v1.1.0 tagged",
-  
-});
-
-var support_11x = gitgraph.branch({
-  parentBranch: master,
-  name: "support/v1.1.x",
-  column: supportCol
-});
-support_11x.commit({
-  message: "Start v1.1.1-rc Release Candidate builds",
- 
-})
-support_11x.commit(bugFixCommit).commit({
-  
-  message: "Release v1.1.1 tagged",
-  
-});
-support_11x.merge(develop);
-develop.commit({
-  messageDisplay: false
-});
-
-var feature4 = gitgraph.branch({
-  parentBranch: develop,
-  name: "feature/4",
-  column: featureCol
-});
-develop.commit({
-  messageDisplay: false
-});
-feature4.commit("A feature to go into v1.2.0").commit({
-  messageDisplay: false
-});
-feature4.merge(develop);
-
-support_11x.commit({
-  message: "Start v1.1.2-rc Release Candidate builds",
- 
-})
-support_11x.commit(bugFixCommit).commit({
- 
-  message: "Release v1.1.2",
-  
-});
-support_11x.merge(develop);
-develop.commit({
-  messageDisplay: false
-});
-
-var feature5 = gitgraph.branch({
-  parentBranch: develop,
-  name: "feature/5",
-  column: featureCol
-});
-develop.commit({
-  messageDisplay: false
-});
-feature5.commit("Another feature to go into v1.2.0").commit({
-  messageDisplay: false
-});
-feature5.merge(develop);
-
-support_11x.commit({
-  message: "Start v1.1.3-rc Release Candidate builds",
- 
-})
-support_11x.commit(bugFixCommit).commit({
-  
-  message: "Release v1.1.3 tagged",
- 
-});
-support_11x.merge(develop);
-develop.commit({
-  messageDisplay: false
-});
-
-var release_120 = gitgraph.branch({
-  parentBranch: develop,
-  name: "release/v1.2.0",
-  column: releaseCol
-});
-release_120.commit({
-  message: "Start v1.2.0-rc Release Candidate builds",
- 
-})
-release_120.commit(stabilizationCommit);
-release_120.merge(develop).merge(master, {
-  
-  message: "Release v1.2.0 tagged",
- 
-});
-develop.commit({
-  messageDisplay: false
-});
-
+displayGraph(test1);
