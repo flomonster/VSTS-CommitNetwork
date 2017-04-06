0ee179c20e026672019af8dde0beef2324d84d22
flomonster <florian.amsallem@epita.fr>
2017-04-06 07:51:33 +0200

    ADD JSON parse

--- scripts/network.js
+++ scripts/network.js
@@ -5,10 +5,11 @@ VSS.require(["VSS/Service", "TFS/VersionControl/GitRestClient"], function (VSS_S
     client.getRepositories().then(
         function(repos)
         {
-           client.getCommits(repos[0].id, {$top: 5}).then(
+           client.getCommits(repos[0].id, {$top: 5}, null, null, 1000).then(
             function(commits)
             {
-              console.log(commits);
+              commits = JSON.stringify(commits);
+              console.log(commits[0]);
             });
 
         });
