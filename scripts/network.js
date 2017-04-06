cd28a8707b2039ab612fcc8724efb4962f147a55
flomonster <florian.amsallem@epita.fr>
2017-04-06 18:27:46 +0200

    ADD creation of graph calling each commits

--- scripts/network.js
+++ scripts/network.js
@@ -5,31 +5,41 @@ VSS.require(["VSS/Service", "TFS/VersionControl/GitRestClient"], function (VSS_S
     client.getRepositories().then(
         function(repos)
         {
-           client.getCommits(repos[0].id, {$top: 5}, null, null, 1000).then(
-            function(commits)
+          var repoId = repos[0].id;
+          client.getCommits(repoId, null, null, null, 1000).then(
+          function(commits)
+          {
+            if (commits.length == 1000)
+              console.log("Too many commits");
+            else
             {
-              	//commits = JSON.stringify(commits);
-              	console.log(commits[0]);
-		        if (commits.length == 1000)
-                    console.log("Too many commits");
-                else
+              var dic = {};
+              for (var i = 0; i < commits.length; i++)
+              {
+                client.getCommit(commits[i].commitId, repoId).then(
+                function(commit)
                 {
-                    console.log(commits[1].parents);
-                    var dic = {};
-                    for (var i = 0; i < commits.length; i++)
-                    {
-                        var merge = commits[i].parents && commits[i].parents.size() > 1;
-                        var date = commits[i].author.date.getFullYear();
-                        date = date * 100 + commits[i].author.date.getMonth();
-                        date = date * 100 + commits[i].author.date.getDate();
-                        date = date * 100 + commits[i].author.date.getHours();
-                        date = date * 100 + commits[i].author.date.getMinutes();
-                        date = date * 100 + commits[i].author.date.getSeconds();
-                        dic[commits[i].commitId] = new Graph(commits[i].commitId, commits[i].author.name, commits[i].comment, date, commits[i].remoteUrl, [], merge);
-                    }
-                }
-            });
-
+                  console.log(commits[i]);
+                  commits[i].parents = commit.parents;
+                  var merge = commits[i].parents.size() > 1;
+                  var date = commits[i].author.date.getFullYear();
+                  date = date * 100 + commits[i].author.date.getMonth();
+                  date = date * 100 + commits[i].author.date.getDate();
+                  date = date * 100 + commits[i].author.date.getHours();
+                  date = date * 100 + commits[i].author.date.getMinutes();
+                  date = date * 100 + commits[i].author.date.getSeconds();
+                  dic[commits[i].commitId] = new Graph(commits[i].commitId, commits[i].author.name, commits[i].comment, date, commits[i].remoteUrl, [], merge);
+                });
+              }
+              for (var i = 0; i < commits.length; i++)
+              {
+                var commit = dic[commits[i].commitId];
+                for (var p in commits[i].parents)
+                  dic[p].children.push(commit);
+              }
+              displayGraph(dic[commits[0].commitId]);
+            }
+          });
         });
       });
 });
