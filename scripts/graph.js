47d7619cdd89b69248f1bd7aa77aec948f16a4e2
flomonster <florian.amsallem@epita.fr>
2017-04-07 20:26:58 +0200

    ADD The working stuff

--- scripts/graph.js
+++ scripts/graph.js
@@ -1,6 +1,6 @@
 class Graph
 {
-  constructor(id, author, msg, date, url, children = [], merge = false)
+  constructor(id, author, msg, date, url, children = [], parents = [], merge = false)
   {
     this.id = id;
     this.author = author;
@@ -8,11 +8,12 @@ class Graph
     this.date = date;
     this.url = url;
     this.children = children;
+    this.parents = parents;
     this.merge = merge;
   }
 }
 
-merge = new Graph("MERGE", "swapme <athawale.1@iitj.ac.in>", "Merge to master", "23/03/2017", "https://www.google.fr/", [], true);
+merge = new Graph("MERGE", "swapme <athawale.1@iitj.ac.in>", "Merge to master", "23/03/2017", "https://www.google.fr/", [], [], true);
 child1 = new Graph("id 2", "swapme <athawale.1@iitj.ac.in>", "ADD readme", "23/03/2017", "", [merge]);
 child2 = new Graph("id 3", "swapme <athawale.1@iitj.ac.in>", "ADD gitignore", "23/03/2017", "https://www.google.es/", [merge]);
 test1 = new Graph("id 1", "swapme <athawale.1@iitj.ac.in>", "Initial Commit", "22/03/2017", "https://extension-westworld.visualstudio.com/_git/COMMIT%20NETWORK/commit/1c054af8d49bf140df6d4253d5ac312579ceb973?refName=refs%2Fheads%2Fmaster", [child1, child2]);
--- scripts/network.js
+++ scripts/network.js
@@ -1,15 +1,5 @@
-function linker(commits, dict)
+function linker(dict)
 {
-  if (commits.length == dict.length)
-  {
-    for (var i = 0; i < commits.length; i++)
-    {
-      var commit = dic[commits[i].commitId];
-      for (var p in commits[i].parents)
-        dic[p].children.push(commit);
-    }
-    displayGraph(dic[commits[0].commitId]);
-  }
 }
 
 VSS.ready(() => {
@@ -19,13 +9,14 @@ VSS.require(["VSS/Service", "TFS/VersionControl/GitRestClient"], function (VSS_S
     client.getRepositories().then(
         function(repos)
         {
-          var repoId = repos[0].id;
+          var repoId = repos[1].id;
           client.getCommits(repoId, null, null, null, 1000).then(
           function(commits)
           {
-            console.log(commits[800]);
             if (commits.length == 1000)
               console.log("Too many commits");
+            else if (commits.length == 0)
+              console.log("Any commits");
             else
             {
               var dic = {};
@@ -34,17 +25,25 @@ VSS.require(["VSS/Service", "TFS/VersionControl/GitRestClient"], function (VSS_S
                 client.getCommit(commits[i].commitId, repoId).then(
                 function(commit)
                 {
-                  console.log(commits[i]);
-                  commits[i].parents = commit.parents;
-                  var merge = commits[i].parents.size() > 1;
-                  var date = commits[i].author.date.getFullYear();
-                  date = date * 100 + commits[i].author.date.getMonth();
-                  date = date * 100 + commits[i].author.date.getDate();
-                  date = date * 100 + commits[i].author.date.getHours();
-                  date = date * 100 + commits[i].author.date.getMinutes();
-                  date = date * 100 + commits[i].author.date.getSeconds();
-                  dic[commits[i].commitId] = new Graph(commits[i].commitId, commits[i].author.name, commits[i].comment, date, commits[i].remoteUrl, [], merge);
-                  linker(commits, dic);
+                  var merge = commit.parents.length > 1;
+                  var date = commit.author.date.getFullYear();
+                  date = date * 100 + commit.author.date.getMonth();
+                  date = date * 100 + commit.author.date.getDate();
+                  date = date * 100 + commit.author.date.getHours();
+                  date = date * 100 + commit.author.date.getMinutes();
+                  date = date * 100 + commit.author.date.getSeconds();
+                  dic[commit.commitId] = new Graph(commit.commitId, commit.author.name, commit.comment, date, commit.remoteUrl, [], commit.parents, merge);
+                  if (commits.length == Object.keys(dic).length)
+                  {
+                    linker(dic);
+                    for (var id in dic)
+                    {
+                      for (var p = 0; p < dic[id].parents.length; p++)
+                        dic[dic[id].parents[p]].children.push(dic[id]);
+                    }
+                    console.log("OK");
+                    displayGraph(dic[commits[commits.length - 1].commitId]);
+                  }
                 });
               }
             }
