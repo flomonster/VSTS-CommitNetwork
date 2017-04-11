edf6468cb66671b7d5972f8f83f929b570c047c9
flomonster <florian.amsallem@hotmail.fr>
2017-04-11 14:52:11 +0200

    ADD pie chart

--- scripts/network.js
+++ scripts/network.js
@@ -15,6 +15,8 @@ VSS.require(["VSS/Service", "TFS/VersionControl/GitRestClient"], function (VSS_S
               console.log("Any commits");
             else
             {
+              var stats = {};
+              var labels = [];
               var dic = {};
               for (var i = 0; i < commits.length; i++)
               {
@@ -29,12 +31,20 @@ VSS.require(["VSS/Service", "TFS/VersionControl/GitRestClient"], function (VSS_S
                   date = date * 100 + commit.committer.date.getMinutes();
                   date = date * 100 + commit.committer.date.getSeconds();
                   dic[commit.commitId] = new Graph(commit.commitId, commit.author.name, commit.comment, date, commit.remoteUrl, [], commit.parents, merge);
+                  if (commit.author.email in stats)
+                    stats[commit.author.email]++;
+                  else
+                  {
+                    stats[commit.author.email] = 1;
+                    labels.push(commit.author.name);
+                  }
                   if (commits.length == Object.keys(dic).length)
                   {
                     for (var id in dic)
                       for (var p = 0; p < dic[id].parents.length; p++)
                         dic[dic[id].parents[p]].children.push(dic[id]);
                     displayGraph(dic[commits[commits.length - 1].commitId]);
+                    displayStats(stats, labels);
                   }
                 });
               }
